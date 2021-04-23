f77bdf8b6563e428718fff79edba98a96d42f5c7
Gaeel Bradshaw <gaeel.bradshaw@goto-games.com>
2021-04-23 08:50:46 +0200

    Adds prettyEventName and PR number

--- dist/index.js
+++ dist/index.js
@@ -6052,6 +6052,11 @@ const github = __nccwpck_require__(569);
 try {
   let event = "Unknown";
   let context = github.context;
+  let pullRequestNumber = "";
+  let prettyEventName = context.eventName
+    .split('_')
+    .map(s=>s.charAt(0).toUpperCase() + s.slice(1))
+    .join(' ');
   switch (context.eventName) {
     case "push":
       event = `Commit ${context.sha.substr(0,8)} pushed by ${context.actor}`
@@ -6060,7 +6065,8 @@ try {
       event = `Manually run by ${context.actor}`
       break;
     case "pull_request":
-      event = `Pull request ${context.ref} by ${context.actor}`
+      pullRequestNumber = /:([^\/]+)/.exec(context.ref)[1];
+      event = `Pull request #${pullRequestNumber} by ${context.actor}`
       break;
     default:
       let prettyEventName = context.eventName
@@ -6070,6 +6076,8 @@ try {
       event = `${prettyEventName} by ${context.actor}`;
   }
   core.setOutput('prettyEvent', event)
+  core.setOutput('prettyEventName', prettyEventName)
+  core.setOutput('PR', pullRequestNumber)
   core.setOutput('runUrl', `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`)
 
 } catch (error) {
--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const github = require('@actions/github');
 try {
   let event = "Unknown";
   let context = github.context;
+  let pullRequestNumber = "";
+  let prettyEventName = context.eventName
+    .split('_')
+    .map(s=>s.charAt(0).toUpperCase() + s.slice(1))
+    .join(' ');
   switch (context.eventName) {
     case "push":
       event = `Commit ${context.sha.substr(0,8)} pushed by ${context.actor}`
@@ -16,7 +21,8 @@ try {
       event = `Manually run by ${context.actor}`
       break;
     case "pull_request":
-      event = `Pull request ${context.ref} by ${context.actor}`
+      pullRequestNumber = /:([^\/]+)/.exec(context.ref)[1];
+      event = `Pull request #${pullRequestNumber} by ${context.actor}`
       break;
     default:
       let prettyEventName = context.eventName
@@ -26,6 +32,8 @@ try {
       event = `${prettyEventName} by ${context.actor}`;
   }
   core.setOutput('prettyEvent', event)
+  core.setOutput('prettyEventName', prettyEventName)
+  core.setOutput('PR', pullRequestNumber)
   core.setOutput('runUrl', `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`)
 
 } catch (error) {
