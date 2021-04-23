bcfb596665a355249a71668bb47960e2c525e7b8
Gaeel Bradshaw <gaeel.bradshaw@goto-games.com>
2021-04-23 08:50:46 +0200

    Adds prettyEventName and PR number

--- dist/index.js
+++ dist/index.js
@@ -6052,15 +6052,22 @@ const github = __nccwpck_require__(569);
 try {
   let event = "Unknown";
   let context = github.context;
+  let pullRequestNumber = "";
+  let prettyEventName = context.eventName
+    .split('_')
+    .map(s=>s.charAt(0).toUpperCase() + s.slice(1))
+    .join(' ');
+  let shortSHA = context.sha.substr(0,8);
   switch (context.eventName) {
     case "push":
-      event = `Commit ${context.sha.substr(0,8)} pushed by ${context.actor}`
+      event = `Commit ${shortSHA} pushed by ${context.actor}`
       break;
     case "workflow_dispatch":
       event = `Manually run by ${context.actor}`
       break;
     case "pull_request":
-      event = `Pull request ${context.ref} by ${context.actor}`
+      pullRequestNumber = /pull\/(.*)\/merge/.exec(context.ref)[1];
+      event = `Pull request #${pullRequestNumber} by ${context.actor}`
       break;
     default:
       let prettyEventName = context.eventName
@@ -6069,8 +6076,12 @@ try {
         .join(' ');
       event = `${prettyEventName} by ${context.actor}`;
   }
-  core.setOutput('prettyEvent', event)
-  core.setOutput('runUrl', `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`)
+  core.setOutput('sha', shortSHA)
+  core.setOutput('event', event)
+  core.setOutput('name', prettyEventName)
+  core.setOutput('pull_request', pullRequestNumber)
+  core.setOutput('run_url', `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`)
+  core.setOutput('pr_url', pullRequestNumber.length>0 ? `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/pull/${pullRequestNumber}` : "")
 
 } catch (error) {
   core.setFailed(error.message);
--- index.js
+++ index.js
@@ -8,15 +8,22 @@ const github = require('@actions/github');
 try {
   let event = "Unknown";
   let context = github.context;
+  let pullRequestNumber = "";
+  let prettyEventName = context.eventName
+    .split('_')
+    .map(s=>s.charAt(0).toUpperCase() + s.slice(1))
+    .join(' ');
+  let shortSHA = context.sha.substr(0,8);
   switch (context.eventName) {
     case "push":
-      event = `Commit ${context.sha.substr(0,8)} pushed by ${context.actor}`
+      event = `Commit ${shortSHA} pushed by ${context.actor}`
       break;
     case "workflow_dispatch":
       event = `Manually run by ${context.actor}`
       break;
     case "pull_request":
-      event = `Pull request ${context.ref} by ${context.actor}`
+      pullRequestNumber = /pull\/(.*)\/merge/.exec(context.ref)[1];
+      event = `Pull request #${pullRequestNumber} by ${context.actor}`
       break;
     default:
       let prettyEventName = context.eventName
@@ -25,8 +32,12 @@ try {
         .join(' ');
       event = `${prettyEventName} by ${context.actor}`;
   }
-  core.setOutput('prettyEvent', event)
-  core.setOutput('runUrl', `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`)
+  core.setOutput('sha', shortSHA)
+  core.setOutput('event', event)
+  core.setOutput('name', prettyEventName)
+  core.setOutput('pull_request', pullRequestNumber)
+  core.setOutput('run_url', `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`)
+  core.setOutput('pr_url', pullRequestNumber.length>0 ? `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/pull/${pullRequestNumber}` : "")
 
 } catch (error) {
   core.setFailed(error.message);
